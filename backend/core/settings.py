723a8241ddf9dfed5c369f4277bdeadea342e7d0
Aarman-hub <arman.dnt84@gmail.com>
2022-07-25 12:56:05 +0600

    Backend core settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -131,6 +131,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+
+MEDIA_URL = "/media/"
+
+
+REST_FRAMEWORK = {}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
